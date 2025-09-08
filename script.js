91ab49231c318a4f22c6b78e6133ef0809ea6c40
Alok kumar <herealok01@gmail.com>
2025-09-08 20:46:50 +0530

    script.js

--- script.js
+++ script.js
@@ -1,89 +1,103 @@
 // Sample materials data (Add as many as you want)
-
-
-
 const materials = [
-  // 1st Year - CSE
-  {
-    year: "1st Year",
-    branch: "CSE",
-    subject: "Physics",
-    title: "Physics Notes - Unit 1",
-    link: "https://drive.google.com/file/d/1ABCDEF123456789/view"
-  },
-  {
-    year: "1st Year",
-    branch: "CSE",
-    subject: "Physics",
-    title: "Physics PPT - Kinematics",
-    link: "https://drive.google.com/file/d/1ZYXWVUT987654321/view"
-  },
-
-  // 2nd Year - CSE
+  // 2nd Year - IT
   {
     year: "2nd Year",
-    branch: "CSE",
-    subject: "DBMS",
-    title: "DBMS Unit 1 PPT",
-    link: "https://drive.google.com/file/d/1DBMS123456789/view"
+    branch: "IT",
+    subject: "Artificial Intelligence",
+    title: "Unit-1",
+    faculty: "Dr. Rajeev Kumar",
+    link: "https://drive.google.com/file/d/1kqheALi7w7v10fnG39yqC1hHVAn_dXEl/view?usp=sharing"
   },
   {
     year: "2nd Year",
-    branch: "CSE",
-    subject: "DBMS",
-    title: "DBMS Assignment 1",
-    link: "https://drive.google.com/file/d/1ASSIGNDBMS98765/view"
-  },
-
-  // 3rd Year - ECE
-  {
-    year: "3rd Year",
-    branch: "ECE",
-    subject: "Digital Electronics",
-    title: "Digital Logic Gates Notes",
-    link: "https://drive.google.com/file/d/1DIGITAL123456789/view"
+    branch: "IT",
+    subject: "Artificial Intelligence",
+    title: "Unit-2",
+    faculty: "Roshni Prasad",
+    link: "https://drive.google.com/file/d/1MclKwdqaw8y6Y8dUIOcp6vPlwFO4nLBs/view?usp=sharing"
   },
   {
-    year: "3rd Year",
-    branch: "ECE",
-    subject: "Digital Electronics",
-    title: "Flip-Flops PPT",
-    link: "https://drive.google.com/file/d/1FLIPFLOP456789/view"
-  },
-
-  // 3rd Year - MECH
-  {
-    year: "3rd Year",
-    branch: "MECH",
-    subject: "Thermodynamics",
-    title: "Thermodynamics - Unit 2",
-    link: "https://drive.google.com/file/d/1THERMO123456789/view"
+    year: "2nd Year",
+    branch: "IT",
+    subject: "AICE",
+    title: "Unit-1",
+    faculty: "Ms. Kumari Kajal Kishore",
+    link: "https://drive.google.com/file/d/14hmSP69f74lurfInC2DgFBVFR9-1aNSl/view?usp=sharing"
   },
   {
-    year: "3rd Year",
-    branch: "MECH",
-    subject: "Thermodynamics",
-    title: "Thermodynamics PPT - Laws of Thermo",
-    link: "https://drive.google.com/file/d/1THERMOPPT98765/view"
-  },
-
-  // 4th Year - CIVIL
-  {
-    year: "4th Year",
-    branch: "CIVIL",
-    subject: "Structural Analysis",
-    title: "Structural Analysis Notes - Beams",
-    link: "https://drive.google.com/file/d/1STRUCT123456789/view"
+    year: "2nd Year",
+    branch: "IT",
+    subject: "AICE",
+    title: "Unit-2",
+    faculty: "Ms. Kumari Kajal Kishore",
+    link: "https://drive.google.com/file/d/1n5-91FGAxE1kC5GFQrcXmzHp2Fb_k6-9/view?usp=sharing"
   },
   {
-    year: "4th Year",
-    branch: "CIVIL",
-    subject: "Structural Analysis",
-    title: "Moment Distribution Method PPT",
-    link: "https://drive.google.com/file/d/1STRUCTPPT98765/view"
+    year: "2nd Year",
+    branch: "IT",
+    subject: "CAPP",
+    title: "Unit-1",
+    faculty: "Ankur Kumar Varshney",
+    link: "https://drive.google.com/file/d/1HlLy-tSUxSo0Gn5uWfrvFaY-Hygi89P_/view?usp=sharing"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "CAPP",
+    title: "Unit-2",
+    faculty: "Dr. Ankita Rani",
+    link: "https://drive.google.com/file/d/10CRoHIkT_OekP2qmvr3erXLtpnDtQNJz/view?usp=sharing"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "DSA",
+    title: "Unit-1",
+    faculty: "VIKRANT MALIK",
+    link: "https://drive.google.com/file/d/1fr4xOYNQWO0RchjobO2Pbnk4ZWku_GYM/view?usp=sharing"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "DSA",
+    title: "Unit-2",
+    faculty: "Dr. Megha Gupta ",
+    link: "https://drive.google.com/file/d/1SlCTbZXPBMySZ-iJLCYo6M5uoRI669Bp/view?usp=sharing"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "Operating System",
+    title: "Unit-1",
+    faculty: "Mr. Atul Garg",
+    link: "https://drive.google.com/file/d/1lsD19I_X9UlsdaiT19e5E-ulGoHnVFWA/view?usp=sharing"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "Technical Communication",
+    title: "Unit-1",
+    faculty: "Not Available",
+    link: "https://drive.google.com/file/d/1VFCGqmEVDKvcbLWyjuJDM4At6puxwdXB/view?usp=drive_link"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "Technical Communication",
+    title: "Unit-2",
+    faculty: "Not Available",
+    link: "https://drive.google.com/file/d/1K5UOi_wOaNVuzL67JbU6BR1Iuc4vOhtD/view?usp=drive_link"
+  },
+  {
+    year: "2nd Year",
+    branch: "IT",
+    subject: "Technical Communication",
+    title: "Unit-3",
+    faculty: "Not Available",
+    link: "https://drive.google.com/file/d/1p9TVlJUq0a4TWhDM1eWOWhnyOE7jh779/view?usp=drive_link"
   }
 ];
-
 
 // Update subjects based on selected year and branch
 const yearSelect = document.getElementById("yearSelect");
@@ -118,8 +132,8 @@ function displayMaterials() {
   const branch = branchSelect.value;
   const subject = subjectSelect.value;
 
-  const filtered = materials.filter((item) =>
-    item.year === year && item.branch === branch && item.subject === subject
+  const filtered = materials.filter(
+    (item) => item.year === year && item.branch === branch && item.subject === subject
   );
 
   materialList.innerHTML = '';
@@ -135,7 +149,8 @@ function displayMaterials() {
 
     card.innerHTML = `
       <h3>${material.title}</h3>
-      <p>Subject: ${material.subject}</p>
+      <p><strong>Subject:</strong> ${material.subject}</p>
+      <p><strong>Faculty:</strong> ${material.faculty}</p>
       <a href="${material.link}" target="_blank">View Material</a>
     `;
 
@@ -155,4 +170,3 @@ branchSelect.addEventListener("change", () => {
 });
 
 subjectSelect.addEventListener("change", displayMaterials);
-
